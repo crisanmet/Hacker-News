dc79186fdfedc69a4d4c5289dbd89caf8d653a3e
Cristian Sancricca <cristiansancricca@gmail.com>
2022-04-12 13:16:42 -0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Cristian Sancricca on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
